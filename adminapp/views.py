33718a28b2b949bc6d205136d551e80b166d054c
tvirus-01 <shakisa54@gmail.com>
2023-06-23 01:54:14 +0600

    tv ads

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,27 @@
+import json
+import random
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.hashers import make_password
+import yfinance as yf
+from datetime import datetime
+import time
+import string, random
+import requests
 
-# Create your views here.
+from userapp.models import *
+from userapp.sendEmails import *
+
+@login_required(login_url='/login')
+def adminDashboard(request):
+    current_user = request.user
+    if not current_user.is_staff:
+        return redirect("/")
+
+    context = {}
+    return render(request, "adminapp/dashboard.html", context=context)
--- base/urls.py
+++ base/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('admin/', include("adminapp.urls")),
     path('', include("userapp.urls"))
 ]
